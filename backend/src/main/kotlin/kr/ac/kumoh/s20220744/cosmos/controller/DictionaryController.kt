b4bfafc54dcd2f667aa5a8cf5fe75c3d36799fac
rkdtjr <dlrkdtjr0116@kumoh.ac.kr>
2025-12-06 00:49:50 +0900

    feat: add Dictionary feature

--- backend/src/main/kotlin/kr/ac/kumoh/s20220744/cosmos/controller/DictionaryController.kt
+++ backend/src/main/kotlin/kr/ac/kumoh/s20220744/cosmos/controller/DictionaryController.kt
@@ -1,4 +1,21 @@
 package kr.ac.kumoh.s20220744.cosmos.controller
 
-class DictionaryController {
+import kr.ac.kumoh.s20220744.cosmos.model.Dictionary
+import kr.ac.kumoh.s20220744.cosmos.service.DictionaryService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/dictionary")
+class DictionaryController(
+    private val dictionaryService: DictionaryService
+) {
+    @GetMapping
+    fun getDictionaryList(): ResponseEntity<List<Dictionary>> = ResponseEntity.ok(dictionaryService.getDictionaryList())
+
+    @GetMapping("/{id}")
+    fun getDictionaryDetail(@PathVariable id: String): ResponseEntity<Dictionary> = ResponseEntity.ok(dictionaryService.getDictionaryDetial(id))
 }
--- backend/src/main/kotlin/kr/ac/kumoh/s20220744/cosmos/service/DictionaryService.kt
+++ backend/src/main/kotlin/kr/ac/kumoh/s20220744/cosmos/service/DictionaryService.kt
@@ -1,11 +1,13 @@
 package kr.ac.kumoh.s20220744.cosmos.service
 
 import kr.ac.kumoh.s20220744.cosmos.repository.DictionaryRepository
+import org.springframework.stereotype.Service
 
+@Service
 class DictionaryService(
     private val dictionaryRepository: DictionaryRepository
 ) {
     fun getDictionaryList() = dictionaryRepository.findAll()
 
-    fun getDictionaryDetial(id: String) = dictionaryRepository.findById(id)
+    fun getDictionaryDetial(id: String) = dictionaryRepository.findById(id).orElse(null)
 }

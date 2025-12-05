c850352dcf45b833eb9acb892d1d50dc93800ef0
rkdtjr <dlrkdtjr0116@kumoh.ac.kr>
2025-12-05 10:51:29 +0900

    feat: Initialize backend project and connect to MongoDB

---
+++ cosmos/src/test/kotlin/kr/ac/kumoh/s20220744/cosmos/CosmosApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220744.cosmos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CosmosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

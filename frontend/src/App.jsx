27e90cfcb1e76825a37c4d8de01e069646bd6a09
rkdtjr <dlrkdtjr0116@kumoh.ac.kr>
2025-12-07 11:39:10 +0900

    feat: update gallery API request & pagination flow, refine UI colors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery"
+import Dictionary from "./pages/Dictionary"
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
+        <Route path="/dictionary" element={<Dictionary />} />
       </Route>
     </Routes>
   );
